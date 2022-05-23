3373d53b27a0be199eea7228fe07e056483b594c
Diego Chavez <ingdiegochavez18@gmail.com>
2022-05-23 14:16:59 -0500

    📦 NEW: add pets components
    ** Add Pets component
    - Form (Create and Edit)
    - List (Show)

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,13 @@ import Home from './components/Home/Home.jsx'
 
 import Categories from './components/Categories/Categories.jsx'
 import FormCategories from './components/Categories/FormCategories.jsx'
+
 import Tags from './components/Tags/Tags.jsx'
 import FormTags from './components/Tags/FormTags.jsx'
+
+import Pets from './components/Pets/Pets.jsx'
+import FormPets from './components/Pets/FormPets.jsx'
+
 
 import './App.css'
 
@@ -24,6 +29,10 @@ function App() {
           <Route exact path="/tags" element={<Tags />} />
           <Route exact path="/tag" element={<FormTags opc="add" />} />
           <Route exact path="/tag/:id" element={<FormTags opc="edit"/>} />
+
+          <Route exact path="/pets" element={<Pets />} />
+          <Route exact path="/pet" element={<FormPets opc="add" />} />
+          <Route exact path="/pet/:id" element={<FormPets opc="edit"/>} />
 
         </Routes>
       </BrowserRouter>
